bda43ab2dcdf7cb7f4a3757539c7b3a3cab1f1fd
Aryazeus123456 <aryaafdhil2002@gmail.com>
2025-02-20 09:58:24 +0700

    `Update package.json and server.js to use nodemon and change port number and routes`

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 3089
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send("Hello express! This is homepage")
+});
+// REQUEST PARAMS
+app.get("/about/:id", (req, res) => {
+  const id = req.params.id
+  res.send('Halo! ini halaman tentang ${id}');
+});
+
+app.get("/blog", (req, res) => {
+    res.send("blog page!")
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`My Personal Web app listening on port ${port}`)
 })
